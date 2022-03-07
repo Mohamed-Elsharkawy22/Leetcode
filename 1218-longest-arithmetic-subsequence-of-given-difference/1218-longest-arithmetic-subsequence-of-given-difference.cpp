8bb30df4a49e429352ae711e32d596344804cdf4
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-03-07 20:00:05 +0200

    Time: 300 ms (25.75%), Space: 54.8 MB (90.62%) - LeetHub

--- 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.cpp
+++ 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.cpp
@@ -5,14 +5,14 @@ public:
         int n=arr.size();
         unordered_map<int,int>dp;
     // for(int i=0;i<n;i++)
-    // dp[arr[i]]=1; // wrong we need the previous values before arr[i] and this may give be next         values
+    // dp[arr[i]]=1; // wrong we need the previous values before arr[i] and this may give be next values        
         
 	int ans=0;
         
 	for(int i=0;i<n;i++){
 		if(dp.count(arr[i]-difference)){// count doesnt create a copy in the map
             
-		  dp[arr[i]]=dp[arr[i]-difference]+1;
+		  dp[arr[i]] = dp[arr[i]-difference]+1;
             
 		}else{
             dp[arr[i]]=1;
