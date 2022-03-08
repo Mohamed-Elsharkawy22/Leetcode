730228f49f59a3bb45caf81ab464460fd43726a3
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-03-08 19:08:44 +0200

    Time: 181 ms (51.58%), Space: 93.4 MB (53.84%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -4,10 +4,24 @@ public:
         
         int mxProfit=0;
         const int n=prices.size();
-        /* mxProfit= (prices[i]-price[j]) ; (i > j) 
+        
+        /*
+           mxProfit= (prices[i]-price[j]) ; (i > j) 
+           ////////////////////////////////
            to maximize the profit we must minimize the price[j] and maximize the price[i]
            price[i] must be a value from prices, so we try all possible values as we traverse
-           maintaing the mnSoFar (price[j]) before the current price[i]*/
+           maintaing the mnSoFar (price[j]) before the current price[i]
+           /////////////////////////////////////////
+           
+           i am now at arbitrary prices[i] 
+           
+          what is the best value for price[j] now?
+          
+          ans: the minimum value before the price[i] 
+           so we maintain the minimum sofar as we traverse the prices 
+            maximizing the profit.
+           
+           */
         
     int mnSoFar=100005; 
     for(int i=0;i<n;i++){ 
