461594c8ba170606ab7593c1f8df59c233895e2f
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-03-08 19:00:35 +0200

    Time: 153 ms (70.75%), Space: 93.3 MB (90.27%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -4,19 +4,17 @@ public:
         
         int mxProfit=0;
         const int n=prices.size();
-        /// mxProfit= (prices[i]-price[j]) ; (i > j) 
-        /// to maximize the profit we must minimize the price[j] and maximize the price[i]
-        /// price[i] must be a value from prices, so we try all possible values as we traverse
-       /// maintaing the mnSoFar (price[j]) before the current price[i]
+        /* mxProfit= (prices[i]-price[j]) ; (i > j) 
+           to maximize the profit we must minimize the price[j] and maximize the price[i]
+           price[i] must be a value from prices, so we try all possible values as we traverse
+           maintaing the mnSoFar (price[j]) before the current price[i]*/
         
     int mnSoFar=100005; 
     for(int i=0;i<n;i++){ 
         mnSoFar=min(mnSoFar, prices[i]);
         mxProfit=max(mxProfit, prices[i]-mnSoFar);
     }
-        
-        
-        
+                
         return mxProfit;
     }
     
