021afc93181ebbd4725ac3c85bbf37052cda62a6
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-03-16 14:19:16 +0200

    Time: 111 ms (27.94%), Space: 39.7 MB (94.80%) - LeetHub

---
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& nums) {
+        /*
+        there is two cases for the  max subarray
+        
+        1-abc **** abc // and this kadane is enough.
+        2-**** abc **** // maxSubarray = totalSum - minSubarray.
+        
+        getting minSubarray by kadane by unverting all array elements into its negative values;
+                      curSum1=max(curSum1-nums[i], -nums[i]);
+
+        */
+        int curSum=0;
+        int best=-30001;
+          int sum=0;
+        for(int i=0;i<nums.size();i++){
+        sum += nums[i];
+            curSum=max(curSum+nums[i],nums[i]);
+
+            best=max(best, curSum);    
+                    
+        }
+        if(best<0) return best; //if(all element are -ve return smallest one)
+        
+        
+           int curSum1=0;
+        int best1=-30001;
+       
+         
+           for(int i=0;i<nums.size();i++){
+              curSum1=max(curSum1-nums[i], -nums[i]);
+              best1=max(best1, curSum1);    
+        }  
+                // we inverted all elements into its negative 
+                // we should restore best1 into positive again by mul by -1 so sum -(-best1)
+                // sum+best1;
+    
+      
+        return max(best, sum + best1);
+        
+    }
+};
