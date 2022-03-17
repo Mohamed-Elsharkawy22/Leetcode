8551b63f719b844964a1a8fa40ccca554ca8f73b
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-03-17 11:54:00 +0200

    Time: 268 ms (75.40%), Space: 109.1 MB (45.25%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -8,7 +8,6 @@ public:
         
         for(int i=0;i<nums1.size();i++){
             for(int j=0;j<nums2.size();j++){
-                
                 if(nums1[i]==nums2[j]){
                     dp[i][j]= (i==0 ||j==0)?1:1+dp[i-1][j-1];
                     
