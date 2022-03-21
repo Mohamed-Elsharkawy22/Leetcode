f7165a15c150a3eea3b9db19e36b3791e5551bc4
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-03-21 18:14:47 +0200

    Time: 0 ms (100.00%), Space: 6.4 MB (96.62%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
+    // find last index of each char
+    // maintain a st to know the start of each segment
+    // maintain (end) to know the max reach for each segment(end of it)
+    //end will be initiate to st (initial condition)
+    // if end == (i (current iteration)), so it is the end of this segment
+    // so st will equal to i+1 and (end will equal to st of this segment(inital condition));
         
         const int n=s.size();
         vector<int>index(26,0);
@@ -8,7 +14,8 @@ public:
             index[s[i]-'a']=i;
             
         
-        int mx=0,st=0;
+        int st=0;
+        int mx=st;
         vector<int>ans;
         
         for(int i=0;i<n;i++){
@@ -16,7 +23,7 @@ public:
             if(mx==i){
                 ans.push_back(i-st+1);
                 st=i+1;
-                mx=i+1;
+                mx=st;
             }
         }
         
