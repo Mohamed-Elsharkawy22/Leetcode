1b89d946d8193f89a4cfbd6736af561d5ef64bda
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-03-21 13:54:38 +0200

    Time: 237 ms (24.36%), Space: 111.7 MB (29.11%) - LeetHub

---
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+        
+        vector<int>top(7,0),
+        bottom(7,0),
+        topAndBottom(7,0);
+        
+        const int n=tops.size();
+        
+        for(int i=0;i<n;i++){
+            
+            top[tops[i]]++;
+            bottom[bottoms[i]]++;
+            
+            if(bottoms[i]==tops[i]){
+                topAndBottom[bottoms[i]]++;
+            }
+            
+        }
+        
+        int minAns=INT_MAX, best=INT_MAX;;
+        for(int i=1;i<=6;i++){
+            
+            if(top[i]+bottom[i]-topAndBottom[i] == n){
+                minAns = n-max(top[i], bottom[i]);
+            }
+            
+            best=min(best, minAns);
+        }
+        
+        
+        return (best!=INT_MAX)?best:-1;
+        
+    }
+};
