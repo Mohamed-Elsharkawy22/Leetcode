7958a3ab896c9ec84435f7681edd8e22cabadb05
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-03-21 15:29:51 +0200

    Time: 182 ms (53.83%), Space: 111.4 MB (99.82%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -2,32 +2,44 @@ class Solution {
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
         
-        vector<int>top(7,0),
-        bottom(7,0),
-        topAndBottom(7,0);
-        
+       
         const int n=tops.size();
+        int swaps=0,
+        minSwaps=INT_MAX;
         
-        for(int i=0;i<n;i++){
-            
-            top[tops[i]]++;
-            bottom[bottoms[i]]++;
-            
-            if(bottoms[i]==tops[i]){
-                topAndBottom[bottoms[i]]++;
-            }
-            
-        }
         
-        int minAns=INT_MAX;
         
         for(int i=1;i<=6;i++){
-            if(top[i]+bottom[i]-topAndBottom[i] == n){
-                minAns =min(minAns, n-max(top[i], bottom[i]));
+            int j=0;
+            swaps=0;
+            
+           for(;j<n;j++){
+              if(tops[j]==i) continue;
+               else if(bottoms[j]==i) swaps++;
+                else break;
             }
+            
+            if(j==n)
+                minSwaps=min(minSwaps, swaps);   
         }
         
-        return (minAns!=INT_MAX)?minAns:-1;
+        for(int i=1;i<=6;i++){
+            int j=0;
+            swaps=0;
+            for(;j<n;j++){
+              if(bottoms[j]==i) continue;
+               else if(tops[j]==i) swaps++;
+                else break;          
+            }
+            
+            if(j==n)
+                minSwaps=min(minSwaps, swaps);   
+        }
+        
+        
+
+        return (minSwaps!=INT_MAX)?minSwaps:-1;
+        
         
     }
 };
