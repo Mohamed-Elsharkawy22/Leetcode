dd712d9da75206b8d9d6bcdcef8b3507dab02180
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-03-22 22:17:04 +0200

    Time: 47 ms (74.67%), Space: 21.1 MB (90.83%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
@@ -6,14 +6,19 @@ public:
         k-=n;
         int index = n-1;
 
+//         while(k>0){
+//            if(k >= 25){ 
+//              ans[index--] = (char) (25+'a');
+//                    k -= 25;
+//            }else{
+//                ans[index] = (char)(k+'a');
+//                k=0;
+//            }
+        
+//         }
         while(k>0){
-           if(k >= 25){ 
-             ans[index--] = (char) (25+'a');
-                   k -= 25;
-           }else{
-               ans[index] = (char)(k+'a');
-               k=0;
-           }
+           ans[index--] += min(25, k);
+             k -= min(25 , k); 
         
         }
     
