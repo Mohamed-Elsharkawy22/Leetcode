5a2891cc527036fad540608633a3dc00c56f5e9c
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-03-30 18:43:40 +0200

    Time: 6 ms (48.48%), Space: 9.5 MB (52.00%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -3,58 +3,31 @@ public:
     
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         
-        int row=bs1(0,matrix.size()-1, matrix,target);
-       
-        if(row==-1){
-            return 0;
-        }
         
-        int found=bs2(0,matrix[0].size()-1, matrix, target,row);
+        int lo=0;
+        int hi= matrix.size()*matrix[0].size()-1;
         
-        if(found==-2) return 0;
-        
-        return 1;
-        
-    }
-    
-    int bs1(int lo, int hi,vector<vector<int>>& matrix, int target){
-        
-        int lstColIndex=matrix[0].size()-1;
-        
-        while(lo<hi){
+        while(lo<=hi){
             
-            int mid=lo+(hi-lo)/2;
-            if(matrix[mid][lstColIndex] >= target ){
-                hi=mid;
-            }else{
-                lo=mid+1;
-            } 
+            int mid = lo+(hi-lo)/2;
             
-        }
-        
-        if(matrix[lo][lstColIndex] >= target)
-            return lo;
-        
-        
-        return -1;
-        
-    }
-    int bs2(int lo, int hi, vector<vector<int>>& matrix, int target,int row){
-        
-        while(lo <= hi){
-            int mid=lo+(hi-lo)/2;
-           
-            if(matrix[row][mid] == target){
-                return -1;
-            }else if(matrix[row][mid] > target){
+            int rIdx=mid/matrix[0].size();
+            int cIdx=mid%matrix[0].size();
+            
+            if(matrix[rIdx][cIdx]==target){
+                return 1;
+            }else if(matrix[rIdx][cIdx]>target){
                 hi=mid-1;
             }else{
                 lo=mid+1;
             }
+            
         }
         
-        return -2;  
+        return 0;
+        
     }
     
+   
     
 };
