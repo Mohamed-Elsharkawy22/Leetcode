1acf0ed9c08b926f093736889d271ca39b9ad5ad
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-04-05 23:23:15 +0200

    Time: 100 ms (77.30%), Space: 59.1 MB (38.83%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -8,7 +8,7 @@ public:
         int j = height.size()-1;
         int mx=0;
         while(i<j){
-            mx=max(mx, (j-i)*min(height[i],height[j]) );
+            mx=max(mx, (j-i) * min(height[i],height[j]) );
             if(height[i] < height[j])
                 i++;
             else
