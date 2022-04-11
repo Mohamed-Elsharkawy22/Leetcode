98ea214591eeb75524aa7c6fae214e5f87047d05
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-04-12 01:47:22 +0200

    Time: 40 ms (54.44%), Space: 14 MB (40.40%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -3,24 +3,21 @@ public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
         const int m=grid[0].size();
         const int n=grid.size();
-        vector<int>ans;
-        k%= (n*m);
-        if(!k) return grid;
-         for(int i= 0; i<n;i++ ){
-            for(int j=0;j<m;j++){
-            ans.push_back(grid[i][j]);
-            }}
-        int idx= (m*n)-k;
+        vector<vector<int>>ans(n,vector<int>(m,0));
+        
         for(int i= 0; i<n;i++ ){
             for(int j=0;j<m;j++){
-              
-                grid[i][j]= ans[idx];
-                idx+= 1;
-                idx%=(m*n);
+                
+                int num= i*m+j;
+                num += k;
+                num %= (n*m); 
+                int nr= num/m;
+                int nc= num%m;
+                ans[nr][nc]=grid[i][j];
             }
         }
         
-        return grid;
+        return ans;
         
     }
 };
