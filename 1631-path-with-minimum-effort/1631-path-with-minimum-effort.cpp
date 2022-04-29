c1f6c42acd792bd819618ac999eba148f5b9e532
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-04-30 01:28:46 +0200

    Time: 196 ms (78.07%), Space: 20 MB (78.94%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        
+        int dx[] ={0,0,-1,1};
+        int dy[] ={1,-1,0,0};
+        
+        const int n = heights.size();
+        const int m = heights[0].size();
+        
+        vector<vector<int>>cost(n,vector<int>(m,INT_MAX));
+        
+        priority_queue< pair<int, pair<int,int> > >pq;
+        
+        
+        cost[0][0]=0;
+        pq.push({0,{0,0}});
+        
+        while(!pq.empty()){
+           
+            pair<int, pair<int,int>> top = pq.top();
+            int c = -1*top.first;
+            int row = top.second.first;
+            int col = top.second.second;
+            pq.pop();
+            
+            if(c > cost[row][col]) continue;
+            
+            for(int i=0;i<4;i++){
+                int nr=row+dx[i];
+                int nc=col+dy[i];
+                
+                if(nr >=0 && nc>=0 && nr < n && nc < m){
+                    
+                    int nCost = max(c, abs(heights[row][col] - heights[nr][nc])); 
+                    
+                    if(nCost < cost[nr][nc]){
+                        cost[nr][nc] = nCost;
+                        pq.push({-nCost,{nr,nc}});
+                    }
+                }
+                
+                
+            }
+            
+            
+            
+        }
+        
+        
+        
+        return cost[n-1][m-1];
+        
+        
+        
+        
+    }
+};
