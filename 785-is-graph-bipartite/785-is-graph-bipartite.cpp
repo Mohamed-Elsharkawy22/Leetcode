f0476c7469429e63d1aa98f993f3222f271f8a36
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-04-29 03:59:07 +0200

    Time: 30 ms (62.15%), Space: 13.4 MB (81.82%) - LeetHub

---
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        const int n=graph.size();
+        int ans=1;
+        vector<int>vis(n,-1);
+        vis[0]=1;
+        for(int i= 0 ;i<n;i++)
+        isBipartite(i,vis,graph,ans);    
+        return ans;
+    }
+
+    void isBipartite(int node, vector<int> &vis,vector<vector<int>>& graph, int &ans){
+        
+        if(!ans) return ;
+        
+        for(int v: graph[node]){
+            
+            if(vis[v] == -1){
+                vis[v] = vis[node]^1;
+                ans &= vis[node] != vis[v]; 
+                isBipartite(v, vis, graph,ans);
+            }else{
+              ans &= vis[node] != vis[v];  
+            }
+            
+           
+        }
+     
+    }
+    
+    
+    
+    
+};
