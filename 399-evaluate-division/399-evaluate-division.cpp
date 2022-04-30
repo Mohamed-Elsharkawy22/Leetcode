2dd6dc4d0af688d3a645b5cfa8468cb55f4fb6ff
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-04-30 04:17:16 +0200

    Time: 9 ms (10.16%), Space: 8 MB (97.30%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.cpp
+++ 399-evaluate-division/399-evaluate-division.cpp
@@ -8,12 +8,12 @@ public:
        // vector<vector<double,double>>cost(2*n,vector<double>(2*n,1.0*INT_MAX));
         for(int i=0;i<50;i++)
             for(int j=0;j<50;j++)
-            cost[i][j]=1.0*INT_MAX;
+               cost[i][j]=1.0*INT_MAX;
+        
         vector<double>ans; 
         
         int idx=0;
         for(int i=0;i<n;i++){
-            
             if(mp.count(equations[i][0]) == 0)
                 mp[equations[i][0]]= ++idx;
              if(mp.count(equations[i][1]) == 0)
@@ -35,7 +35,7 @@ public:
             
             for(int i=1;i<=mp.size();i++){
                 for(int j=1;j<=mp.size();j++){
-                    cost[i][j]=min( cost[i][j], cost[i][k]*cost[k][j]);
+                    cost[i][j]=min(cost[i][j], cost[i][k]*cost[k][j]);
                 }
             }
         }
@@ -43,10 +43,9 @@ public:
         for(int i=0;i<queries.size();i++){
              int n1=mp[queries[i][0]];
                 int n2=mp[queries[i][1]];
-            if(mp.count(queries[i][0])==0 || mp.count(queries[i][1])==0 ||cost[n1][n2]>= 1000.0 )
+            if(mp.count(queries[i][0])==0 || mp.count(queries[i][1])==0 ||cost[n1][n2]>= 500 )
                 ans.push_back(-1.0);
             else{
-                   
                 ans.push_back( cost[n1][n2]);
             }
         }
