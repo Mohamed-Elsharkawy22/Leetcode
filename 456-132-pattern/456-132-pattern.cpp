71a8719bced40eafdbbba0eac71830a7cb629f54
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-05-07 21:35:24 +0200

    Time: 107 ms (38.92%), Space: 38.3 MB (69.05%) - LeetHub

--- 456-132-pattern/456-132-pattern.cpp
+++ 456-132-pattern/456-132-pattern.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
-        int s3=-2e9-5;
+        int s3=INT_MIN;
         stack<int>st;
         const int n=nums.size();
         for(int i=n-1;i>=0;--i){
