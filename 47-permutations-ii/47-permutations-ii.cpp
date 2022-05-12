5dc8128de334854e0e742964deb54569341af6ab
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-05-13 00:20:31 +0200

    Time: 7 ms (80.91%), Space: 8.4 MB (89.32%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -2,48 +2,49 @@ class Solution {
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         
-        vector< int >temp;
-        vector< vector< int> > ans;
-      unordered_map<int,int>count;
+        vector< int > temp;
+        vector< vector< int > > ans;
         
-        for(int i=0;i<nums.size();i++){
+        unordered_map< int, int > count;
+        
+        const int sz= nums.size();
+        
+        for(int i=0;i<sz;i++){
             count[nums[i]]++;
         }
-        int sz=nums.size();
-        bt(temp, ans, sz,count);
-       
-        return ans;
         
+        permute(temp, ans, count, sz);
+        
+        return ans;
     }
     
-    void bt(vector<int> &temp, vector<vector<int>> &ans,int sz  ,unordered_map<int,int> &count){
+    
+    void permute(vector<int> &temp, vector<vector<int>>&ans, unordered_map< int, int > &count, int sz ){
         
         if(temp.size()==sz){
+            
             ans.push_back(temp);
             return ;
         }
         
-        for(auto pair: count){
-            int val=pair.first;
-          if(count[val]==0) continue;
-            temp.push_back(val);
-            count[val]--;
-            bt(temp,ans,sz,count);
-             count[val]++;
-            temp.pop_back();            
-        }
         
-        
-        
+       for(auto pair: count){
+           int val = pair.first;
+           if(count[val] == 0) continue;
+           
+           temp.push_back(val);
+           count[val]--;
+           permute(temp, ans, count, sz);
+           count[val]++;
+           temp.pop_back();      
+           
+           
+       }
         
         
         
     }
     
     
+    
 };
-
-
-
-
-
