50f6e6f66a3b972886775d7ef9e48067a7f72ea3
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-05-13 16:23:36 +0200

    Time: 8 ms (37.62%), Space: 11.9 MB (99.90%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-     
+        
         int i=nums.size()-2;
-        
-        while(i>=0 && nums[i] >= nums[i+1])
+        while(i>=0 && nums[i]>= nums[i+1]){
             i--;
-        
+        }
         
         if(i>=0){
             int j=nums.size()-1;
-            while(nums[i] >= nums[j])
-                j--;
             
-           swap(nums,i,j);
+            while(nums[i] >= nums[j]){
+                j--;
+            }
+            swap(nums[i], nums[j]);
         }
-        reverse(nums,i+1);
+        
+        reverse(nums, i+1);
+    
     }
     
-    void swap(vector<int>& nums,int i ,int j){
-        int val=nums[i];
-        nums[i]=nums[j];
-        nums[j]=val;
-    }
-    void reverse(vector<int>& nums, int start){
-        int j=nums.size()-1;
-        int i=start;
-        while(i<j){
-            swap(nums,i,j);
-            i++,j--;
+    
+    
+    void reverse(vector<int>&nums, int st){
+        
+        int en=nums.size()-1;
+        
+        while(st<en){
+            swap(nums[st], nums[en]);
+            st++;
+            en--;
         }
+        
     }
+    
     
 };
