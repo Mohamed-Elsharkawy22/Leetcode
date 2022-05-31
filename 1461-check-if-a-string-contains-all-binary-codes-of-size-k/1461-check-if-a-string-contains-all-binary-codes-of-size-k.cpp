f68b61622fb6627864d83b716bd04405e5fbe1a6
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-05-31 23:32:23 +0200

    Time: 222 ms (80.43%), Space: 40 MB (77.02%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -3,15 +3,15 @@ public:
     bool hasAllCodes(string s, int k) {
         const int n= (int)s.size();
         if(n<k) return 0;
-        unordered_set<int>treeSet;
+        unordered_set<int>hashSet;
         int mask=0;
         int all=(1<<k)-1; // k ones
         for(int i=0;i<n;i++){
             mask = ((mask<<1) & all) | (s[i]-'0');
             if(i>=k-1)
-                treeSet.insert(mask);
+                hashSet.insert(mask);
         }
         
-        return treeSet.size()==(1<<k);
+        return hashSet.size()==(1<<k);
     }
 };
