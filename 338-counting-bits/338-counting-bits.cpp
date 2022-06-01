b5f6174bdece44132eb53a7bb77cff1935d68297
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-06-01 14:16:34 +0200

    Time: 18 ms (6.37%), Space: 8.6 MB (21.30%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
-    
-   // i & (i-1) --->>> removes the first one of the right from i;
-    // i tsbq (i&(i-1) )be one move 
-    
     vector<int> countBits(int n) {
-        vector<int> dp(n+1,0);
-        for(int i=1;i<=n;i++)
-           dp[i] = dp[i&(i-1)]+1;
+        //nlogn
         
-        return dp;
+        vector<int>bits;
+        
+        for(int i=0;i<=n;i++){
+            int cnt=0;
+            int num=i;
+            while(num){
+              if(num%2!=0){  
+                  cnt++;
+              }
+                num/=2;
+            }
+            bits.push_back(cnt);
+            
+        }
+        return bits;
     }
-    
-    
-    
 };
+
+
+   
