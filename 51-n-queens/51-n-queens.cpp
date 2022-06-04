e5267236f72c4774bdd85735753c9251971125b3
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-06-04 10:50:10 +0200

    Time: 4 ms (88.77%), Space: 7.2 MB (67.79%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     vector<vector<string>> solveNQueens(int n) {
-        vector<int>cols(n,0),dig1(2*n,0),dig2(2*n,0);
+        int cols=0,dig1=0,dig2=0;
+        
         vector<string>board(n, string(n,'.'));
         vector<vector<string>> ans;
         permute(n,0,ans,board,cols,dig1,dig2);
@@ -10,7 +11,7 @@ public:
     }
     
     
-    void permute(const int n,int row, vector<vector<string> >&ans,vector<string>&board, vector<int>&cols,vector<int>&dig1, vector<int>&dig2){
+    void permute(const int n,int row, vector<vector<string> >&ans,vector<string>&board, int cols,int dig1, int dig2){
         
         if(row==n){
             ans.push_back(board);
@@ -19,13 +20,19 @@ public:
         
         for(int col=0;col<n;col++){
             
-            if(cols[col] || dig1[col+row] || dig2[col-row+n-1]) continue;
+            if( (cols & (1<<col)) || (dig1&(1<<(row+col))  || (dig2 & (1<< (col-row+n-1) )) ) )                               continue;
             
             board[row][col]='Q';
-            cols[col]=dig1[col+row]=dig2[col-row+n-1]=1;
+            cols |=(1 <<col);
+            dig1 |= (1<<(row+col));
+            dig2 |= (1<< (col-row+n-1) );      
+               
             permute(n,row+1,ans,board,cols,dig1,dig2);
+            
             board[row][col]='.';
-            cols[col]=dig1[col+row]=dig2[col-row+n-1]=0;
+            cols &= ~(1<<col);
+            dig1 &= ~(1<<(row+col));
+            dig2 &= ~(1<< (col-row+n-1) );           
     
         }
         
