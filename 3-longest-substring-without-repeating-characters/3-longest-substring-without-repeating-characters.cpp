9cbcaef52393844205161d52800865c097453d28
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-06-10 09:49:06 +0200

    Time: 27 ms (50.21%), Space: 10.7 MB (30.93%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -2,17 +2,21 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         
+      unordered_set<char>in;
+        int st=0;
         int ans=0;
-        const int n=s.size();
-        unordered_set<char> lookup;
-        int start=0;
-        for(int i=0;i<n;i++){
-            
-           while(lookup.find(s[i]) != lookup.end()){
-               lookup.erase(s[start++]);
-           } 
-           lookup.insert(s[i]);
-            ans=max(ans,(int)lookup.size());
+        for(int i=0;i<s.size();i++){
+            if(in.find(s[i])==in.end()){
+                in.insert(s[i]);
+            }else{
+                
+               while(in.find(s[i])!=in.end()){
+                   in.erase(s[st]);
+                   st++;
+               }
+                in.insert(s[i]);
+            }
+            ans=max(ans, i-st+1);
         }
         
         return ans;
