f47680156590c0b515924017f2ec6263d17e4c09
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-06-12 00:33:26 +0200

    Time: 182 ms (89.18%), Space: 98.5 MB (91.02%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -7,14 +7,16 @@ public:
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
         }
+        if(sum < x) return -1;
         
-        int target=sum-x;
+        int target = sum-x;
         
         if(target==0) return nums.size();
+        
         int fast=0,slow=0;
         
         int cum=0;
-        int longest=0;
+        int longest=-1;
         while( slow<nums.size()){
             
             while(fast<nums.size()  && cum < target ){
@@ -26,7 +28,9 @@ public:
             cum-= nums[slow];
             slow++;
         }
-        if(longest==0) return -1;
+        
+  if(longest==-1) return -1;
+        
         return nums.size()-longest;
         
     }
