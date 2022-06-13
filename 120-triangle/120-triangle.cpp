e80fa483916f521554e3c4faa5a60873f90f78a3
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-06-13 11:47:29 +0200

    Time: 16 ms (11.39%), Space: 8.5 MB (98.91%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -3,28 +3,16 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
        
         const int n= triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        
-        
-        return minimumTotalSolve(triangle,  0,  0, dp);
-    }
-    
-    
-    int minimumTotalSolve(vector<vector<int>>& triangle, int i, int j, vector<vector<int>>&dp){
-        
-        if(i == triangle.size()){
-            
-            return 0;
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1]); 
+            }
         }
         
-        int &ret=dp[i][j];
-        if(~ret) return ret;
-        
-        ret=0;
-        ret= triangle[i][j] + minimumTotalSolve(triangle, i+1, j, dp);
-        ret=min(ret,triangle[i][j]+ minimumTotalSolve(triangle, i+1, j+1, dp));
-        return ret;
-        
+        return triangle[0][0];
     }
+    
+    
+   
     
 };
