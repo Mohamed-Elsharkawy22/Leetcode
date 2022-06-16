df9c873c373bec073d3bff5b73a0c87ded2b266e
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-06-16 09:30:57 +0200

    Time: 201 ms (52.05%), Space: 231.6 MB (19.58%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,34 +1,39 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-     int ans=0;
-        int st=0,en=0;
+        
+   
+        int l=0,r=0,ans=0;
         for(int i=0;i<s.size();i++){
-          int len1=expandAroundCenter(s,i,i);
-           int len2= expandAroundCenter(s,i,i+1);
-            int len=max(len1,len2);
+             
+            int ans1=expandAroundCenter(s,i,i);
+            int ans2=expandAroundCenter(s,i,i+1);    
             
-            if(len>ans){
-                ans=len;
-                st=i- (len-1)/2;
-                en= i+(len/2);
-            }
-            
+            int mx=max(ans1,ans2);
+            if(mx>ans){
+                ans=mx;
+                l= i-((ans+1)/2)+1;
+                r= i+(ans/2);   
+            }   
         }
         
-      return s.substr(st,en-st+1);  
-}
+        return s.substr(l, r-l+1);
+        
+    }
     
-    int expandAroundCenter(string &s, int Left, int Right){
-            int l=Left, r=Right;
-            
-            while(l>=0 && r<s.size()&& s[l]==s[r]){
-                l--;
-                r++;
-            }
-         /// stbbben and the actual string is bbb so len = r-l-1
-            return r - l - 1 ;
+    
+    int expandAroundCenter(string s, int left ,int right){
+        
+        int l=left, r=right;
+        
+        while(l>=0 && r<s.size() && s[l] == s[r]){
+            l--;
+            r++;
         }
+        return r-l-1;
+    }
     
-
+    
+    
+    
 };
