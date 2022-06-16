58a3d1c3a413add282ba186d4694c671b94d863b
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-06-16 18:15:32 +0200

    Time: 881 ms (12.93%), Space: 387.3 MB (5.33%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,37 +1,44 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        
-   
-        int l=0,r=0,ans=0;
-        for(int i=0;i<s.size();i++){
-             
-            int ans1=expandAroundCenter(s,i,i);
-            int ans2=expandAroundCenter(s,i,i+1);    
-            
-            int mx=max(ans1,ans2);
-            if(mx>ans){
-                ans=mx;
-                l= i-((ans+1)/2)+1;
-                r= i+(ans/2);   
-            }   
+        int n=s.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        int ans=0;
+        int fst=0,lst=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(palindrome(s,i,j,dp)){
+                    int sz=j-i+1;
+                    if(ans<sz){
+                        ans=sz;
+                        fst=i, lst=j;
+                    }
+                }
+            }
         }
         
-        return s.substr(l, r-l+1);
+       
+       return s.substr(fst, lst-fst+1);
         
     }
     
+   bool palindrome(string &s,int i, int j, vector<vector<int>>& dp){
+       if(i==j) return 1;
+       
+       if(i+1==j) return s[i]==s[j];
+       
+       int &ret=dp[i][j];
+       if(~ret)
+           return ret;
+       ret=0;
+       
+       if(s[i]==s[j])
+           ret=palindrome(s,i+1,j-1,dp);
+      
+       return ret;
+   }
     
-    int expandAroundCenter(string &s, int left ,int right){
-        
-        int l=left, r=right;
-        
-        while(l>=0 && r<s.size() && s[l] == s[r]){
-            l--;
-            r++;
-        }
-        return r-l-1;
-    }
+    
     
     
     
