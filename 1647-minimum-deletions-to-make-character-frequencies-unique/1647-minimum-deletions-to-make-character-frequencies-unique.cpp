58de9ffce76b55bf439fd8f4f22cbb4bf5b059f0
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-06-29 22:31:44 +0200

    Time: 108 ms (36.55%), Space: 17.5 MB (34.25%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -2,25 +2,24 @@ class Solution {
 public:
     int minDeletions(string s) {
         
-        map<char,int>mpToFreq;
-        map<int,int>countFreq;
+        unordered_map<char,int>mpToFreq;
+        unordered_map<int,int>countFreq;
         
-        for(int i=0;i<s.size();i++){
+        for(int i=0;i<s.size();i++){ //n
             mpToFreq[s[i]]++;
         }
-        for(pair<char, int>val: mpToFreq){
+        for(pair<char, int>val: mpToFreq){ // 26
              countFreq[ mpToFreq[val.first] ]++;
-        
         }
         
         int cnt=0;
-        for(pair<char,int> val: mpToFreq){
+        for(pair<char,int> val: mpToFreq){ // < 26*n
            // cout<<"ff "<<(char)val.first<<endl;
             while( mpToFreq[val.first]>0 && countFreq[ mpToFreq[val.first] ] > 1){
                 
                // cout<<(char)val.first<<endl;
                 countFreq[ mpToFreq[val.first]]--;
-                mpToFreq[val.first]--;
+                mpToFreq[val.first]--; 
                 countFreq[ mpToFreq[val.first]]++;
                 cnt++;
             }
