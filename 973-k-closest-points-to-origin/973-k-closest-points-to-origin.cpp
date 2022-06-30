d2425323b6dee8f9cf4ce8690760e614fc1f82f1
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-06-30 14:23:43 +0200

    Time: 1306 ms (6.24%), Space: 234.3 MB (5.04%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
       
-        sort(points.begin(), points.end(),[&](vector<int>v1, vector<int>v2){
+        sort(points.begin(), points.end(),[](vector<int>v1, vector<int>v2){
           int x1=v1[0], y1=v1[1];
           int x2=v2[0], y2= v2[1];
            
