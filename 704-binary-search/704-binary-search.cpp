186b61ac3a19140300a096dc8e3990290b7e3eca
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-06-30 14:34:24 +0200

    Time: 63 ms (33.49%), Space: 27.5 MB (63.82%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -3,22 +3,20 @@ public:
     int search(vector<int>& nums, int target) {
         
         
-        int lo=0;
-        int hi=nums.size()-1;
+        int lo=0, hi=nums.size()-1;
         
-        while(lo <= hi){
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
             
-            int mid=lo+ (hi-lo+1)/2;
-            
-            if(nums[mid] == target)
+            if(nums[mid]==target)
                 return mid;
-            
-            else if(nums[mid] > target)
+            else if(nums[mid]>target){
                 hi=mid-1;
-            else
+            }else{
                 lo=mid+1;
+            }
         }
         
         return -1;
-    }
+        }
 };
