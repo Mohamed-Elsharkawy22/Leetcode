a6501acb0e061ad5f2101477e5569e905cf55959
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-06-30 10:11:41 +0200

    Time: 26 ms (75.70%), Space: 10.8 MB (77.88%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -2,42 +2,30 @@ class Solution {
 public:
     string minRemoveToMakeValid(string s) {
         
-        int count=0;
+      int cnt=0;  
         
-        for(int i=0;i<s.size();i++){
-            
-            if(s[i]=='('){
-                count++;
-            
-            }else if(count>0 && s[i]==')'){
-                count--;
-            }else if(s[i]==')'){
-                s[i]='?';
-                
-            }
+      const int n=s.size();
+        
+        for(int i=0;i<n;i++){
+            if(s[i]=='(') cnt++;
+             else if(cnt > 0 && s[i]==')') cnt--;
+             else if(cnt==0 && s[i]==')') s[i]='*'; 
+        }
+        cnt=0;
+         for(int i=n-1;i>=0;i--){
+            if(s[i]==')') cnt++;
+             else if(cnt > 0 && s[i]=='(') cnt--;
+             else if(cnt==0 && s[i]=='(') s[i]='*'; 
+        }
+        
+        string res="";
+        for(int i=0;i<n;i++){
+            if(s[i]!='*') res.push_back(s[i]);
             
         }
-        count=0;
-            for(int i=s.size()-1;i>=0;i--){
-            
-            if(s[i]==')'){
-                count++;
-            
-            }else if(count>0 && s[i]=='('){
-                count--;
-            }else if(s[i]=='('){
-                s[i]='?';
-                
-            }
-            
-        }
-        string ans="";
-        for(int i=0;i<s.size();i++){
-            
-            if(s[i]!='?')ans.push_back(s[i]);
-        }
-        return ans;
         
-        
+        return res;
     }
 };
+
+
