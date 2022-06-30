963a43d44209d102d3b7b52e190e0e7ba2871408
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-06-30 10:19:48 +0200

    Time: 29 ms (66.23%), Space: 11.8 MB (39.99%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -5,23 +5,30 @@ public:
       int cnt=0;  
         
       const int n=s.size();
+       stack<int>st;
         
         for(int i=0;i<n;i++){
-            if(s[i]=='(') cnt++;
-             else if(cnt > 0 && s[i]==')') cnt--;
-             else if(cnt==0 && s[i]==')') s[i]='*'; 
-        }
-        cnt=0;
-         for(int i=n-1;i>=0;i--){
-            if(s[i]==')') cnt++;
-             else if(cnt > 0 && s[i]=='(') cnt--;
-             else if(cnt==0 && s[i]=='(') s[i]='*'; 
+            
+            if(s[i]=='('){
+                st.push(i);
+            }else if(s[i]==')') {
+                if(st.empty()){
+                    s[i]='*';
+                }else{
+                    st.pop();
+                }
+            }
+            
         }
         
+        while(!st.empty()){
+            s[st.top()]='*';
+            st.pop();
+        }
         string res="";
         for(int i=0;i<n;i++){
-            if(s[i]!='*') res.push_back(s[i]);
-            
+            if(s[i]!='*')
+                res.push_back(s[i]);
         }
         
         return res;
