6532534671b9a3624bb020e32900b3f30f2cdf12
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-06-30 10:43:54 +0200

    Time: 61 ms (77.78%), Space: 19.7 MB (76.34%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,33 +1,41 @@
 class Solution {
 public:
     
-    bool isPalindrome(string s, int i, int j){
+    bool isPalin(string &s, int st, int en){
         
-        while(i < j){
-            if(s[i] != s[j]) return false;
+        while(st<=en && s[st]==s[en]){
             
-            i++,j--;
+            st++;
+            en--;
         }
-     
-        return true;
+        if(en<=st) return 1;
+        
+        return 0;
     }
     
     
     bool validPalindrome(string s) {
         
-        int i=0,
-            j= s.size()-1;
+        int pt1=0, pt2=s.size()-1;   
         
-        while(i < j){
+     const int n=s.size();
+        
+        int cnt=0;
+        while(pt1<=pt2){
             
-            if(s[i] != s[j]){
-                return ( (isPalindrome(s,i,j-1)) || (isPalindrome(s,i+1,j)) );
+          
+            
+            if(s[pt1]==s[pt2]){
+                pt1++;
+                pt2--;
+            }else{
+                cnt++;
+                if(cnt>1)
+                    return 0;
+              
+                return (isPalin(s,pt1+1,pt2) | isPalin(s,pt1,pt2-1));
             }
-            i++,j--;
         }
-        
-        return true;
+        return 1;
     }
 };
-
-
