7d7686cdaafcfc2c8d7db79b3783d4b2c302e643
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-07-01 12:00:44 +0200

    Time: 13 ms (30.87%), Space: 14.1 MB (35.63%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -1,39 +1,43 @@
 class Solution {
-
 public:
     int search(vector<int>& nums, int target) {
-     
-        int lo=0;
-        int hi=nums.size()-1;
         
+        int lo=0, hi=nums.size()-1;
         
-        while(lo <= hi){
-            
-            int mid = lo+(hi-lo)/2;
-            
-            if(nums[mid] == target)
-                return 1;
-        
-            if(nums[mid] > nums[lo] ){   
-               
-                //[lo-------mid] is ordered;
-                if(nums[mid] > target && target >= nums[lo])
-                    hi=mid-1;
-                else
-                    lo=mid+1;   
-            
-            }else if(nums[mid] < nums[lo]){ //if(nums[mid] < nums[hi]) w nshel else
-                //[mid......hi] is ordered
-                if(nums[mid] < target && target <= nums[hi])
+        while(lo<=hi){
+                while (lo < hi && nums[lo] == nums[lo + 1])
+        ++lo;
+      while (lo < hi && nums[hi] == nums[hi - 1])
+        --hi;
+            int mid=lo+(hi-lo)/2;
+            if(nums[mid] >= nums[0]){
+                
+                if(target >= nums[0]){
+                    if(nums[mid]==target)
+                        return 1;
+                    else if(nums[mid] > target)
+                        hi=mid-1;
+                    else
+                        lo=mid+1;
+                }else{
                     lo=mid+1;
-                else
+                }
+            }else{
+                if(target >= nums[0]){
                     hi=mid-1;
-            }else
-                lo++;
-                  
+                }else{
+                    if(nums[mid]==target)
+                        return 1;
+                    else if(nums[mid] > target)
+                        hi=mid-1;
+                    else
+                        lo=mid+1;
+                }
+            }            
         }
         
         return 0;
+        
+        
     }
-
 };
