62823e3a785a0439c6bddc8a51820d4247349bd6
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-07-01 22:06:51 +0200

    Time: 11 ms (48.90%), Space: 14 MB (36.10%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -2,46 +2,41 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         
-        int lo=0, hi=nums.size()-1;
+        int lo=0, hi= nums.size()-1;
+        
         
         while(lo<=hi){
             
-            while (lo < hi && nums[lo] == nums[lo + 1])
-                 ++lo;
-            while (lo < hi && nums[hi] == nums[hi - 1])
-                 --hi;
+            while(lo<hi && nums[lo]==nums[lo+1])lo++;
+            while(lo<hi && nums[hi]==nums[hi-1])hi--;
             
             int mid=lo+(hi-lo)/2;
             
-              if(nums[mid]==target)
-                        return 1;
+            if(nums[mid]==target)
+                return 1;
             
-            
-            if(nums[mid] >= nums[0]){
-                
+            if(nums[mid] >= nums[0] ){
                 if(target >= nums[0]){
-                    if(nums[mid] > target)
-                        hi=mid-1;
-                    else
-                        lo=mid+1;
+                     if(nums[mid]>target)
+                         hi=mid-1;
+                      else
+                          lo=mid+1;
                 }else{
                     lo=mid+1;
                 }
-            }else{
                 
-                if(target >= nums[0]){
-                    hi=mid-1;
-                }else{
-                    if(nums[mid] > target)
-                        hi=mid-1;
-                    else
-                        lo=mid+1;
-                }
-            }            
+            }else{
+                 if(target < nums[0] ){
+                  if(nums[mid]>target)
+                         hi=mid-1;
+                 else
+                          lo=mid+1;
+            }else{
+                    hi=mid-1; 
+                 } 
+        
         }
-        
+        }
         return 0;
-        
-        
     }
 };
