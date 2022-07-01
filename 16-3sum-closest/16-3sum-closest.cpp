3ba19672ff53c30243a7b66270358b5eb2b9c1f2
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-07-01 14:22:22 +0200

    Time: 576 ms (21.10%), Space: 25.7 MB (19.88%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -12,22 +12,18 @@ public:
           int pt2=nums.size()-1;
           
           while(pt1<pt2){
-              
               int subSum=nums[i]+nums[pt1]+nums[pt2];
               if(abs(target-subSum) < mndiff){
                   mndiff=abs(target-subSum);
                   mnSum=subSum;
               }
-              if(subSum==target){
-                  
-                  while(pt1<pt2 && nums[pt1]==nums[pt1+1]) pt1++;
-                  while(pt1<pt2 && nums[pt2]==nums[pt2-1]) pt2--;
-                  
-                  pt1++,pt2--;
-              }else if(subSum > target)
+              if(subSum > target)
                      pt2--;
-              else
+              else if(subSum < target)
                   pt1++;
+              else{
+                  return subSum;
+              }
           }
       }
           
