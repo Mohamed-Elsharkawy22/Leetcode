8e04daf06c7cb08f9a0d47e3a4501605d31d89ec
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-07-03 20:16:22 +0200

    Time: 4 ms (94.38%), Space: 15.1 MB (7.93%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,51 +11,59 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-
-      if(list1 && !list2 ){
-          
-          return list1;
-      }
-        if(list2 && !list1){
-            return list2;
+        
+    
+      ListNode* ans=nullptr;
+      ListNode* ptr=ans;
+        
+         while(list1!=nullptr and list2 != nullptr){
+             
+             if(list1->val <= list2->val){
+                 
+                 if(ans==nullptr){
+                    ans = new ListNode(list1->val);
+                    ptr=ans; 
+                 
+                 }else{
+                     ptr->next=new ListNode(list1->val);
+                     ptr=ptr->next;
+                 }
+      list1=list1->next;
+             }else{
+                 if(ans==nullptr){
+                  ans = new ListNode(list2->val);
+                    ptr=ans; 
+                 
+                 }else{
+                     ptr->next=new ListNode(list2->val);
+                     ptr=ptr->next;
+                 }
+              list2=list2->next;   
+             }             
+         }
+        
+        while(list1!=nullptr){
+            if(ans==nullptr){
+                  ans = new ListNode(list1->val);
+                    ptr=ans; 
+                 
+                 }else{
+                     ptr->next=new ListNode(list1->val);
+                     ptr=ptr->next;
+                 }
+            list1=list1->next;
         }
-        
-        if(!list1 && !list2) return NULL;
-        
-        ListNode* sortedList=new ListNode(),
-        *ptr = sortedList;
-        
-        
-        while(list1 && list2){    
-        
-            if(list1->val <= list2->val){
-                
-                ptr->next= new ListNode(list1->val);
-                ptr=ptr->next;
-                list1=list1->next;
-                
-            }else{
-                
-                ptr->next= new ListNode(list2->val);
-                ptr=ptr->next;
-                list2=list2->next;
-            }
-               
-        }
-        
-        while(list1){
-             ptr->next= new ListNode(list1->val);
-                ptr=ptr->next;
-                list1=list1->next;
-            
-        }
-        while(list2){
-                ptr->next= new ListNode(list2->val);
-                ptr=ptr->next;
-                list2=list2->next;
-        }
-        
-   
-        return sortedList->next;
+    while(list2 != nullptr){
+        if(ans==nullptr){
+                  ans = new ListNode(list2->val);
+                    ptr=ans; 
+                 
+                 }else{
+                     ptr->next=new ListNode(list2->val);
+                     ptr=ptr->next;
+                 }
+        list2=list2->next;
+    }
+    return ans;
     }
 };
