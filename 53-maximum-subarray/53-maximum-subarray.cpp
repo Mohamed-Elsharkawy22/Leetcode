df8b0d9bbfad3888a43227a828922640c1848a6d
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-07-04 21:47:18 +0200

    Time: 252 ms (5.03%), Space: 67.8 MB (10.82%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -2,12 +2,42 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         
-        int ans=-1e4 -5;
-        int subSum=0;
-        for(int i=0;i<nums.size();i++){
-            subSum=max(nums[i], subSum+nums[i]);
-            ans=max(ans, subSum);
-        }
-    return ans;
+     return mxSub(0,(int)nums.size()-1, nums);
     }
+    
+    int mxSub(int left, int right, vector<int>& nums){
+        
+        if(left==right)
+            return nums[left];
+        
+        int mid=left+(right-left)/2;
+        int leftSum=mxSub(left,mid,nums);
+        int rightSum=mxSub(mid+1,right,nums);
+        
+        ///cross sum // we must include (so start) nums[mid] and nums[mid+1]; 
+         int totSum=nums[mid];
+         
+          int bestLeftCross=nums[mid];
+        for(int i=mid-1;i>=left;i--){
+            totSum += nums[i];
+            if(totSum > bestLeftCross)
+                bestLeftCross=totSum;
+        }
+       
+        totSum=nums[mid+1];
+        int bestRightCross=nums[mid+1];
+        for(int i=mid+2;i<=right;i++){
+            totSum += nums[i];
+            if(totSum > bestRightCross )
+                bestRightCross=totSum;
+        }
+        ////////
+        
+       return max({leftSum, rightSum, bestRightCross+bestLeftCross}); 
+    }
+    
+    
+    
+    
+    
 };
