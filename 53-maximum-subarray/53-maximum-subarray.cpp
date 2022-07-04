11babc7e524049adbba7771a452e47b902c0cdc6
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-07-04 20:36:46 +0200

    Time: 241 ms (6.57%), Space: 67.7 MB (53.51%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -2,19 +2,12 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         
-        int n=nums.size();
-        vector<long long> pfx(n + 1,0);  // prefix sum array initially filled with 0's
-	for (int i = 1; i <= n; i++){
-		pfx[i] = pfx[i - 1] + nums[i-1];  // compute the prefix sum at each element
-	}
-
-	long long max_subarray_sum =pfx[1] ;
-	long long min_prefix_sum = 0;
-	for (int i = 1; i <= n; i++) {
-		// Max subarray sum is the maximum difference between two prefix sums
-		max_subarray_sum = max(max_subarray_sum, pfx[i] - min_prefix_sum);
-		min_prefix_sum = min(min_prefix_sum, pfx[i]);
-	}
-        return max_subarray_sum;
+        int ans=-1e4 -5;
+        int subSum=0;
+        for(int i=0;i<nums.size();i++){
+            subSum=max(nums[i], subSum+nums[i]);
+            ans=max(ans, subSum);
+        }
+    return ans;
     }
 };
