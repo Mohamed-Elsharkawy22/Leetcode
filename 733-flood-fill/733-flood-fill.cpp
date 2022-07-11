a124b9ed355b29b438a19a106fd9513be1a4fe7c
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-07-11 14:41:05 +0200

    Time: 28 ms (5.19%), Space: 14.3 MB (26.28%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,55 +1,43 @@
 class Solution {
     private:
-    int n;
-    int m;
-    
-     bool valid(int i,int j,vector<vector<int>>& image, int newColor,int oldColor,
-               vector<vector<int>>& vis){
-         
-       return (i>=0 && i < n && j>=0 && j< m &&
-              !vis[i][j] &&image[i][j]==oldColor); 
-    }
-    
-    const int dx[4]={1,-1,0,0};
+     const int dx[4]={1,-1,0,0};
     const int dy[4]={0,0,1,-1};
-    
-     void dfs(int i,int j, int newColor,int oldColor ,vector<vector<int>>& image,
-             vector<vector<int>>& vis){
-
-         
-          
-        image[i][j]=newColor;
-         vis[i][j]=1;
-         
-        for(int k=0;k<4;k++){
-            int ni=i+dx[k];
-            int nj=j+dy[k];
-            
-            if(!valid(ni, nj, image,newColor,oldColor,vis))
-                 continue;
-            dfs(ni, nj, newColor, oldColor, image,vis);
-        }
-        
+    bool valid(int nx, int ny, vector<vector<int>>& image,vector<vector<int>>& vis,int oldColor ){
+        return (nx >=0 && nx <image.size() && ny>=0 && ny<image[0].size() &&!vis[nx][ny] && 
+              image[nx][ny]==oldColor);
     }
+    
+     void dfs(int R,int C, vector<vector<int>>& image, int color, int oldColor, vector<vector<int>> &vis ){
+        
+        for(int i=0;i<4;i++){
+            int nx = R+dx[i];
+            int ny = C+dy[i];   
+            
+            if(valid(nx,ny,image, vis, oldColor)){
+                vis[nx][ny]=1;
+                image[nx][ny]=color;
+                dfs(nx, ny, image, color, oldColor, vis);
+            }   
+        }  
+    }
+    
+    
+    
+    
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+  
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int m=image.size();
+        int n=image[0].size();
+        vector<vector<int>>vis(m, vector<int>(n,0));
         
-      n=image.size();
-      m=image[0].size();  
+        vis[sr][sc]=1;
         
-        vector<vector<int>>vis(n,vector<int>(m,0));
-       
-        int oldColor=image[sr][sc];
-        
-        dfs(sr, sc, newColor, oldColor,image,vis); 
-       
+        dfs(sr, sc, image, color, image[sr][sc], vis);
+        image[sr][sc]=color;
         return image;
     }
     
-   
     
    
-    
-    
-    
 };
