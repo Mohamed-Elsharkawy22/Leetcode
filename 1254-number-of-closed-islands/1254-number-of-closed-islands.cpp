32b3f81790f96d07055cf25a64c73e16e7aca93a
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-07-13 11:16:19 +0200

    Time: 22 ms (41.73%), Space: 9.7 MB (28.07%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,42 +1,35 @@
 class Solution {
-    int n;
-    int m;
-    const int dx[4]={0,0,-1,1};
-    const int dy[4]={1,-1,0,0};
-   bool dfs(int i,int j, vector<vector<int>>& grid){
-      
-       if(i < 0 || i > n-1 || j < 0 || j> m-1)
-                  return 0;
-           
-           if(grid[i][j]==1 || grid[i][j]==-1) return 1;
-           
-         if(i == 0 || i == n-1 || j == 0 || j== m-1) // i have 0
-                  return 0;
-           
-           grid[i][j]=-1; 
-           
-       bool ans=1;
-       for(int k=0;k<4;k++){
-           int ni=i+dx[k];
-           int nj=j+dy[k];
-           ans &= dfs(ni,nj,grid);
-       }
-       
-       return ans;
-   }
-    
-public:
+      const int dx[4]={1,-1,0,0};
+      const int dy[4]={0,0,1,-1};
+    public:    
     int closedIsland(vector<vector<int>>& grid) {
-        n=grid.size();
-        m=grid[0].size();
+        int n=grid.size();
+        int m=grid[0].size();
         int ans=0;
-        
-        
+        queue<pair<int,int>>q;
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==0)
-                ans += dfs(i,j,grid);
-            }
+          for(int j=0;j<m;j++){
+              if(grid[i][j]==0){
+                  q.push({i,j});
+                  bool isClosed=1;
+                  while(!q.empty()){
+                      auto top=q.front();
+                      q.pop();
+                      if((top.first ==0 || top.first ==n-1 || top.second ==0 ||top.second==m-1))                                     isClosed=0;  
+                      
+                      for(int k=0;k<4;k++){
+                          int ni=top.first+dx[k];
+                          int nj=top.second+dy[k];
+                          if(ni >=0 && ni<n &&nj >=0 &&nj<m &&grid[ni][nj]==0){
+                               grid[ni][nj]=1; // visitied
+                                q.push({ni,nj});
+                            }
+                      }
+                 }
+                if(isClosed)
+                    ans++;
+              }  
+          }  
         }
         return ans;
     }
