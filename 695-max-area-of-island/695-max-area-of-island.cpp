fd316e96a44e84f6be1674cef2a4c43b8d327ed7
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-07-15 21:45:00 +0200

    Time: 21 ms (84.06%), Space: 23.2 MB (89.29%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -3,24 +3,24 @@ class Solution {
     int m;
      const int dx[4]={0,0,-1,1};
      const int dy[4]={1,-1,0,0};
-     void dfs(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& vis, int &ans ){
+     void dfs(int i, int j, vector<vector<int>>& grid, int &ans ){
         
          for(int k=0;k<4;k++){
              int ni=i+dx[k];
              int nj=j+dy[k];
              
-             if(valid(ni,nj,grid,vis))
+             if(valid(ni,nj,grid))
              {
                  ans++;
-                 vis[ni][nj]=1;
+                 grid[ni][nj]=0;
                  
-                 dfs(ni,nj,grid,vis,ans);
+                 dfs(ni,nj,grid,ans);
              }
          } 
     };
     
-    bool valid(int i, int j, vector<vector<int>>& grid,vector<vector<int>>& vis ){
-        return (i>=0 && i<n && j>=0 && j<m && grid[i][j]==1 && !vis[i][j]);
+    bool valid(int i, int j, vector<vector<int>>& grid ){
+        return (i>=0 && i<n && j>=0 && j<m && grid[i][j]==1 );
     }
     
 public:
@@ -29,15 +29,15 @@ public:
        m=grid[0].size();
         int ans=0;
         int cnt=0;
-        vector<vector<int>>vis(n,vector<int>(m,0));
+        
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==0 || vis[i][j]==1) continue;
+                if(grid[i][j]==0 ) continue;
                 
                  ans++;
-                 vis[i][j]=1;
-                dfs(i,j,grid,vis,ans);
+                 grid[i][j]=0;
+                dfs(i,j,grid,ans);
                 cnt=max(cnt, ans);
                 ans=0;
             }
