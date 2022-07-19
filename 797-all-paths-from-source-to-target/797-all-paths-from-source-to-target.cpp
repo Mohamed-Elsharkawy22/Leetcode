e1308f3005e5311e80a894ebac04a4205fd10346
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-07-19 19:21:02 +0200

    Time: 8 ms (97.74%), Space: 10.6 MB (93.71%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
-    void path(int pos,vector<int> ds,vector<vector<int>> graph,vector<vector<int>>& ans){
-        if(pos==graph.size()-1){
-            ans.push_back(ds);
-            return;
-        }
-        for(int i=0;i<graph[pos].size();i++){
-            ds.push_back(graph[pos][i]);
-            path(graph[pos][i],ds,graph,ans);
-            ds.pop_back();
-        }            
-    }
-    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        vector<vector<int>> ans;
-        path(0,{0},graph,ans);
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph)
+    {
+        vector<vector<int>>ans;
+        vector<int>path;
+        path.push_back(0);
+        dfs(0,graph,ans,path);
+        
         return ans;
     }
+    
+    
+    void dfs(int node, vector<vector<int>>& graph, vector<vector<int>>& ans, vector<int>&path)
+    {
+        
+        if(node == graph.size()-1)
+        {
+            ans.push_back(path);
+            return;
+        }
+        
+        for(int val : graph[node])
+        {
+            path.push_back(val);
+            dfs(val,graph,ans,path);
+            path.pop_back();
+        }
+        
+    }
+    
+    
 };
