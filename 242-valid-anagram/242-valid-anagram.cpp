3d49229658163bd2b428e64b73d351d10ccdd4f7
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-07-28 15:50:31 +0200

    Time: 30 ms (7.78%), Space: 7.5 MB (7.79%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,9 +1,27 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+      
+        unordered_map<char,int>mp;
         
-        return s==t;
+        int sz=0;
+        for(int i=0;i<s.size();i++)
+            mp[s[i]-'a']++;
+        
+        
+        for(int i=0;i<t.size();i++)
+        {
+            if(mp.count(t[i]-'a')==0) 
+                return 0;
+            if(mp[t[i]-'a'] > 1) 
+                mp[t[i]-'a']--;
+            else
+            {
+            mp.erase(t[i]-'a');
+            }
+        }   
+        
+        return mp.size()==0;
+        
     }
 };
