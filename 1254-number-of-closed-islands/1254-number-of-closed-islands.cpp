967971d0cb94a4c91ab5b9bd8c14cf4d57cd9bf6
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-08-01 00:19:10 +0200

    Time: 16 ms (75.29%), Space: 13.8 MB (9.81%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,37 +1,54 @@
 class Solution {
-      const int dx[4]={1,-1,0,0};
-      const int dy[4]={0,0,1,-1};
-    public:    
+    private:
+    const int dx[4] = {0,0,1,-1};
+    const int dy[4] = {1,-1,0,0};
+    
+public:
     int closedIsland(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
+        const int n = grid.size();
+        const int m = grid[0].size();
+        
         int ans=0;
-        queue<pair<int,int>>q;
-        for(int i=0;i<n;i++){
-          for(int j=0;j<m;j++){
-              if(grid[i][j]==0){
-                  q.push({i,j});
-                  grid[i][j]=1;//visited
-                  bool isClosed=1;
-                  while(!q.empty()){
-                      auto top=q.front();
-                      q.pop();
-                      if((top.first ==0 || top.first ==n-1 || top.second ==0 ||top.second==m-1))                                     isClosed=0;  
-                      
-                      for(int k=0;k<4;k++){
-                          int ni=top.first+dx[k];
-                          int nj=top.second+dy[k];
-                          if(ni >=0 && ni<n &&nj >=0 &&nj<m &&grid[ni][nj]==0){
-                               grid[ni][nj]=1; // visitied
-                                q.push({ni,nj});
-                            }
-                      }
-                 }
-                if(isClosed)
-                    ans++;
-              }  
-          }  
-        }
-        return ans;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j] == 0)
+                {
+                    
+                    queue<pair<int,int>>q;
+                    q.push({i,j});
+                    grid[i][j]=1;
+                    bool isBorder=0;
+                    while(!q.empty())
+                    {
+                        auto tp = q.front();
+                        q.pop();
+                        
+                        if(tp.first == 0 || tp.first == n-1 || tp.second == 0 || tp.second == m-1)
+                            isBorder=1;
+                            
+                       
+                        for(int k=0;k<4;k++)
+                        {
+                           int ni = tp.first+dx[k];
+                           int nj = tp.second+dy[k];
+                           
+                           if(ni < 0 || ni > n-1 || nj < 0 || nj > m-1 || grid[ni][nj]==1)
+                                  continue;
+                             
+                           q.push({ni,nj});
+                           grid[ni][nj]=1;
+                           
+                       }
+  
+                    }
+                             
+                     if(!isBorder)
+                         ans++;
+                }
+            }
+        } 
+        return ans; 
     }
 };
