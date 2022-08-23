260ea10970ba6190bc2eebb036e129f01ec8e006
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-08-23 16:30:13 +0200

    Time: 542 ms (45.35%), Space: 122.2 MB (58.15%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        vector<vector<int>>adj(n, vector<int>());
+        for(int i=0;i<n;i++){
+             if(manager[i]!=-1)
+                adj[manager[i]].push_back(i);
+        }
+        return dfs(headID, manager, informTime, adj);
+    }
+    
+    
+    int dfs(int node, vector<int>& manager, vector<int>& informTime, vector<vector<int>>&adj )           {
+        int maxChildCost=0;
         
-        int sz = manager.size();
-        vector<vector<int>> adjacencyList(sz, vector<int>());
-        
-        for(int i=0; i<sz; i++){
-            if(manager[i] == -1) continue;
-            adjacencyList[manager[i]].push_back(i);
+        for(int v: adj[node]){           
+            maxChildCost=max(maxChildCost, dfs(v,manager,informTime,adj));
         }
         
-        return dfs(headID, adjacencyList, informTime);
+        return maxChildCost+informTime[node];
     }
     
-    
-int dfs(int id, vector<vector<int>>& adjlist, vector<int>& informTime){
-   
-    if(adjlist[id].size()==0) return 0;
-    
-    int maxtime = 0;
-    vector<int> subvertices = adjlist[id];
-    
-    for(int i=0; i<subvertices.size(); i++){
-        maxtime = max(maxtime, dfs(subvertices[i], adjlist, informTime));
-    }
-    
-    return maxtime + informTime[id];
-} 
 };
