a4cb1db78ed8b76c92074b994c6cf4186597ca9d
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-08-25 19:33:06 +0200

    Time: 6 ms (63.99%), Space: 45.6 MB (77.23%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+   
+         int[] freq = new int[26];
+                  
+         for(int i=0;i<26;i++){
+             freq[i]=0;
+         }
+        
+        for(int i=0;i<magazine.length();i++){
+            freq[magazine.charAt(i)-'a']++;
+        }
+        
+        for(int i=0;i<ransomNote.length();i++){
+            
+            if(freq[ransomNote.charAt(i)-'a'] <= 0 )
+                 return false;
+            
+           freq[ransomNote.charAt(i)-'a']--; 
+        }
+        
+        return true;  
+}
+
+}
