aef79b1c1bee4db62aa4cbe94da488c3c6607a7d
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-09-15 19:58:10 +0200

    Time: 390 ms (20.79%), Space: 53.9 MB (38.12%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> ans;
-        vector<int> out(n);
-        //make a reverse adj list(transpose of a graph)
-        vector<int> adj[n];
         
-        for(int i=0;i<n;i++){
-            for(auto nbr: graph[i]){
-                out[i]++;
-                adj[nbr].push_back(i);
+        vector<int>ans;
+        unordered_map<int, int> safe;
+        
+         
+         for(int i=0;i<graph.size();i++)
+             dfs(i,graph,safe,ans);
+        
+         for(int i=0;i<graph.size();i++)
+            {
+             if(safe[i])
+             ans.push_back(i);
             }
-        }
+         return ans;
+    
+    } 
+    
+    bool dfs(int node,vector<vector<int>>& graph, unordered_map<int, int> &safe, vector<int>&ans){
         
-        //take the terminal nodes in the queue
-        queue<int> q;
-        for(int i=0;i<n;i++){
-            if(out[i]==0){
-                q.push(i);
-            }
-        }
+        if(safe.count(node)>0)
+            return safe[node];
         
-        while(!q.empty()){
-            int top = q.front(); q.pop();
-            ans.push_back(top);
-            for(auto nbr: adj[top]){
-                out[nbr]--;
-                //out[nbr] will be zero for a node if and only if all of its edges make it to a safe node 
-                //or terminal node, take those nbr in the queue
-                if(out[nbr]==0) q.push(nbr);
-            }
-        }
+           safe[node]=0;
+
+          for(int v:graph[node]){
+              if(!dfs(v,graph,safe,ans))
+                  return false;
+          }
         
-        //sort, required.
-        sort(ans.begin(), ans.end());
-        return ans;
+        safe[node]=1;
+        return 1;
+        
+        
     }
+    
+    
 };
