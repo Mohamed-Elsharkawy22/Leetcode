47de76f72ed935bdfa91c34471956162cac46d4c
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-09-20 13:27:37 +0200

    Time: 642 ms (29.95%), Space: 109 MB (59.41%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
+        const int n=nums1.size();
+        const int m=nums2.size();
         
-        vector<vector<int>>dp(nums1.size(),vector<int>(nums2.size(),0));
+        int mx=0;
         
-        int ans=0;
+        vector<vector<int>>dp(n, vector<int>(m,0));
         
-        for(int i=0;i<nums1.size();i++){
-            for(int j=0;j<nums2.size();j++){
-                if(nums1[i]==nums2[j]){
-                    dp[i][j]= (i==0 ||j==0)?1:1+dp[i-1][j-1];
-                    
-                    ans=max(ans,dp[i][j]);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(nums1[i] == nums2[j]){
+                    dp[i][j] = ((i == 0 || j == 0 )?1:1+dp[i-1][j-1]);
+                    mx=max(dp[i][j], mx);
                 }
             }
         }
         
-        return ans;
+        return mx;
     }
 };
