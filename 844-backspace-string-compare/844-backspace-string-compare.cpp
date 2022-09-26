9ebc19667f84edb86c6ba493c6eaf88c2887b216
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-09-26 21:14:45 +0200

    Time: 3 ms (55.83%), Space: 6.1 MB (89.83%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,22 +1,52 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-     
-        return newString(s)==newString(t);
+      
         
-    }
-    
-    string newString(string s){
-        string newS="";
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='#'){
-                if(!newS.empty())
-                    newS.pop_back();
+        int i=s.size()-1, j=t.size()-1;
+        
+        int skips=0, skipt=0;
+        
+        while(i>=0 || j>=0){
+            
+            while(i>=0)
+            {   
+              if(s[i]=='#'){
+                 i--,skips++;
+                
+              }else if(skips){
+                 i--;
+                 skips--;
+              }else
+                 break;
+            }
+            
+            while(j>=0)
+            {
+                
+            if(t[j]=='#'){
+                j--,skipt++;
+            }else if(skipt){
+                  skipt--;
+                  j--;   
             }else
-                newS.push_back(s[i]);
+                break;
+            }
+            
+            if(i>=0&&j>=0&& s[i]!= t[j])
+                return 0;
+            
+            if(i<0 && j>=0 || j<0 && i>=0)
+                   return 0;
+            
+           
+            
+            i--,j--;
         }
         
-        return newS;
+        return 1;
     }
+    
+   
     
 };
