1b08524bc0e388f5bf1c141e1deb1168bac64412
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-09-26 21:20:55 +0200

    Time: 6 ms (79.93%), Space: 7.9 MB (47.68%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
-    
-    // 01111 odd ->>> /2 ->>>>00111 ->>> right shift drop first one from right;
-    //odd number loss one setted bit
-    // odd number has more numbers of ones by one than i/2; bcz when i did i/2 i dropped the first one 
-    
-    
-    // 01110 even ->>> /2 ->>>> 00111 -->>> right shift only lose zero not one
-    // enven number has same number of bits like i/2;
     vector<int> countBits(int n) {
-        //n
-        vector<int>bits(n+1,0);
-        
+        vector<int>dp(n+1,0);
+        dp[0]=0;
         for(int i=1;i<=n;i++){
-         if(i%2==0)
-             bits[i]= bits[i/2];
-         else
-            bits[i]= bits[i/2]+1; 
+            dp[i]=dp[i/2];
+            if(i%2!=0)
+                dp[i]++;
         }
-        return bits;
+        
+        return dp;
+        
     }
-}; 
-
-   
+};
