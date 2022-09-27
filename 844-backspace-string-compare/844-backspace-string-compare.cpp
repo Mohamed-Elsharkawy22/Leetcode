feed0be99fdeb5ed3c1d4bec2ee9349458d80247
Mohamed El-Sharkawy <64488126+Mohamed-Elsharkawy22@users.noreply.github.com>
2022-09-27 16:04:41 +0200

    Time: 4 ms (40.10%), Space: 6.1 MB (89.90%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -3,48 +3,50 @@ public:
     bool backspaceCompare(string s, string t) {
       
         
-        int i=s.size()-1, j=t.size()-1;
         
+        int i =s.size()-1, j = t.size()-1;
         int skips=0, skipt=0;
         
         while(i>=0 || j>=0){
             
-            while(i>=0)
-            {   
-              if(s[i]=='#'){
-                 i--,skips++;
+            while(i>=0){
                 
-              }else if(skips){
-                 i--;
-                 skips--;
-              }else
-                 break;
+                if(s[i]=='#')
+                {
+                    i--, skips++;
+                }else if(skips){
+                    i--;
+                    skips--;
+                }else{
+                    break;
+                }
+            }
+            while(j>=0){
+                
+                if(t[j]=='#')
+                {
+                    j--, skipt++;
+                }else if(skipt){
+                    j--;
+                    skipt--;
+                }else{
+                    break;
+                }
             }
             
-            while(j>=0)
-            {
-                
-            if(t[j]=='#'){
-                j--,skipt++;
-            }else if(skipt){
-                  skipt--;
-                  j--;   
-            }else
-                break;
-            }
-            
-            if(i>=0&&j>=0&& s[i]!= t[j])
+            if(i>=0 && j<0 || j>=0 && i<0 )
                 return 0;
             
-            if(i<0 && j>=0 || j<0 && i>=0)
-                   return 0;
             
-           
+            if(i>=0 && j>=0 && s[i]!=t[j])
+                return 0;
             
             i--,j--;
         }
         
+        
         return 1;
+        
     }
     
    
