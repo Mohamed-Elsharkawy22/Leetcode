b06e19fa37c1d878553cdf397dfdf260919fabf2
Mohammad Elsharqawy <64488126+MohammadElsharqawy@users.noreply.github.com>
2023-10-23 18:40:05 +0300

    Time: 0 ms (100%), Space: 6.5 MB (13.31%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n == 1)
+            return true;
+
+        if(n<= 0)
+            return false;    
+
+        double x = log(n)/log(4);
+
+        return (x==int(x));   // check if divisible
+    }
+};
