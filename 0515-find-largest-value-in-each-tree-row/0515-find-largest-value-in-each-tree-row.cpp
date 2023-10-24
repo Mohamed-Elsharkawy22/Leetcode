a6a41fa9dca62b8951ab7af1c92edf79e26c6eb3
Mohammad Elsharqawy <64488126+MohammadElsharqawy@users.noreply.github.com>
2023-10-24 13:47:07 +0300

    Time: 16 ms (9.36%), Space: 22.3 MB (63%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
@@ -13,30 +13,50 @@ class Solution {
 public:
     vector<int> largestValues(TreeNode* root) {
         vector<int>values;
-        if(!root) return values;
+        // if(!root) return values;
 
 
-        queue<TreeNode*>q;
+        // queue<TreeNode*>q;
        
-        q.push(root);
+        // q.push(root);
 
-        while(!q.empty()){
-            int sz = q.size();
-            int mx = INT_MIN;
-            while(sz--){
-                auto top = q.front();
-                q.pop();
-                mx = max(mx, top->val);
-                if(top->left != NULL){
-                    q.push(top->left);
-                }
-                if(top->right != NULL){
-                    q.push(top->right);
-                }
-            }
-            values.push_back(mx);
-        }
+        // while(!q.empty()){
+        //     int sz = q.size();
+        //     int mx = INT_MIN;
+        //     while(sz--){
+        //         auto top = q.front();
+        //         q.pop();
+        //         mx = max(mx, top->val);
+        //         if(top->left != NULL){
+        //             q.push(top->left);
+        //         }
+        //         if(top->right != NULL){
+        //             q.push(top->right);
+        //         }
+        //     }
+        //     values.push_back(mx);
+        // }
 
+        dfs(root, 0, values);
         return values;
+    }
+
+
+
+    void dfs(TreeNode* node, int depth, vector<int> &values){
+        if(!node)
+         return ;
+
+
+         if(depth == values.size()) {// first node in new row
+            values.push_back(node->val);
+         }else{
+             values[depth] = max(values[depth], node->val);
+         }   
+
+
+         dfs(node->left, depth+1, values);
+         dfs(node->right, depth+1, values);
+
     }
 };
